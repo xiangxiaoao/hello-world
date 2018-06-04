cda01a44b92501218990019e3d8c59f31b371bd6
xiangxiaoao <1272902981@qq.com>
2018-06-05 00:04:17 +0800

    commit a java class to hello-world

---
+++ src/com/tang/test/HelloTest.java
@@ -0,0 +1,7 @@
+package com.tang.test;
+
+public class HelloTest {
+    public static  void main(String[] args){
+        System.out.println("to Test commit a file through the GigHub Desktop application!");
+    }
+}
