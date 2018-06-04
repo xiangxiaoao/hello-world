ec1312616b2d045778f6b385d6bd491a5692264c
xiangxiaoao <1272902981@qq.com>
2018-06-05 00:09:58 +0800

    update

--- src/com/tang/test/HelloTest.java
+++ src/com/tang/test/HelloTest.java
@@ -2,6 +2,8 @@ package com.tang.test;
 
 public class HelloTest {
     public static  void main(String[] args){
-        System.out.println("to Test commit a file through the GigHub Desktop application!");
+        System.out.println("to Test commit a file through the GitHub Desktop application!");
+
+        System.out.println("to Test update a file to the GitHub");
     }
 }
